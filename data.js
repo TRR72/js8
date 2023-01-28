cfbe0c0555fa6c67683a9252676eceaaa4308ca4
GFW_72 <116944430+TRR72@users.noreply.github.com>
2023-01-28 15:39:32 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+const dataInfo = `[
+    {
+        "id": "ab1",
+        "url": "https://oir.mobi/uploads/posts/2021-05/thumbs/1620827983_2-oir_mobi-p-zhivie-oboi-s-zhivotnimi-zhivotnie-krasivo-2.jpg",
+        "width": 450,
+        "height": 450,
+        "description": "Сильный тигр",
+        "name": "Тигр 1"
+    },
+    {
+        "id": "ab2",
+        "url": "https://oir.mobi/uploads/posts/2021-05/thumbs/1620828036_52-oir_mobi-p-zhivie-oboi-s-zhivotnimi-zhivotnie-krasivo-54.jpg",
+        "width": 450,
+        "height": 450,
+        "description": "Всем привет!",
+        "name": "Тигр 2"
+    },
+    {
+        "id": "ab3",
+        "url": "https://oir.mobi/uploads/posts/2021-05/thumbs/1620828054_12-oir_mobi-p-zhivie-oboi-s-zhivotnimi-zhivotnie-krasivo-12.jpg",
+        "width": 450,
+        "height": 450,
+        "description": "Хочу купить пантеру",
+        "name": "Котик черный"
+    },
+    {
+        "id": "ab4",
+        "url": "https://oir.mobi/uploads/posts/2021-05/thumbs/1620828055_45-oir_mobi-p-zhivie-oboi-s-zhivotnimi-zhivotnie-krasivo-47.jpg",
+        "width": 450,
+        "height": 450,
+        "description": "Бурая панда",
+        "name": "Панда"
+    },
+    {
+        "id": "ab5",
+        "url": "https://oir.mobi/uploads/posts/2021-05/thumbs/1620828014_10-oir_mobi-p-zhivie-oboi-s-zhivotnimi-zhivotnie-krasivo-10.jpg",
+        "width": 450,
+        "height": 450,
+        "description": "Хочу спать",
+        "name": "Кот спит"
+    }
+    ]`
